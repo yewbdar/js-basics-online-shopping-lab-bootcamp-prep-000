7199372ba4f7d526bd3b0d9edec08faeab45d2c8
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-26 18:19:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,14 +58,14 @@ function removeFromCart(item) {
   if(cart.length > 0){
     for(let i=0; i<cart.length; i++){
       if(cart[i].itemName === item){
-         return cart.splice(i,1);
+          cart.splice(i,1);
         found=false;
       } 
     }
     if(found){
       return 'That item is not in your cart.'
     }
-    
+    return cart
   }
 }
 
