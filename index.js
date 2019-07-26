8e5e02521b471a4d620dc8f1c4d6107a69d14163
Yewbdar Abera <yewbdar.grima@gmail.com>
2019-07-26 18:17:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,14 +54,13 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  let found=true;
   if(cart.length > 0){
     for(let i=0; i<cart.length; i++){
       if(cart[i].itemName === item){
           cart.splice(i,1);
-        
-      } else{
-        return 'The item is not in your cart.'
-       }
+        found=false;
+      } 
     }
      
     return cart
